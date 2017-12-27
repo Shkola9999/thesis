36e1d94a25699618da2e661a9eb3420bce5c9d52
Shkola9999 <Shkola9999@rambler.ru>
2017-12-27 12:35:38 +0100

    Initial Commit

---
+++ MessengerProjectVistula/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessengerProjectVistula
+//
+//  Created by Yevhen Shkola on 27.12.2017.
+//  Copyright © 2017 Yevhen Shkola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
